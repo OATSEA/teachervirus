e7b3022ca98d2395e3012bbcae2fbfcf165e1813
harrylongworth <harry@harrylongworth.com>
2015-05-25 22:01:28 +1000

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -3,16 +3,22 @@
 // create a page of buttons for initial functions
 
 $debug=1;
+echo "<!DOCTYPE html><html><title>Admin Area</title></head><body>";
 echo "<h1>Admin Area</h1>";
 $currentFilePath = realpath(dirname(__FILE__));
 $dir = new RecursiveDirectoryIterator($currentFilePath,FilesystemIterator::SKIP_DOTS );
 foreach(new RecursiveIteratorIterator($dir,RecursiveIteratorIterator::SELF_FIRST) as $file) {
 	$filename= $file->getFilename();
-	if ($filename !="index.php") {
-	  $justname = substr($filename,0,-3);
-		echo "<p><a href='$filename'>$justname</a></p>";	
-	}
+	if (!is_dir($filename)) {
+		if ($filename !="index.php") {
+			$justname = substr($filename,0,-4);
+			echo "<p><a href='$filename'>$justname</a></p>";	
+		}
+	} // end is dir
+
 } // END foreach
+
+echo "<hr></body></html>";
 
 ?>
 
