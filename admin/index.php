65f2f122b283d93c5343d9838d03ef7edc488b4d
harrylongworth <harry@harrylongworth.com>
2015-05-25 21:56:20 +1000

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,18 @@
 <?php
 // Need to check if password has been set on the admin folder, if not then create it
 // create a page of buttons for initial functions
-echo "Admin Area";
+
+$debug=1;
+echo "<h1>Admin Area</h1>";
+$currentFilePath = realpath(dirname(__FILE__));
+$dir = new RecursiveDirectoryIterator($currentFilePath,FilesystemIterator::SKIP_DOTS );
+foreach(new RecursiveIteratorIterator($dir,RecursiveIteratorIterator::SELF_FIRST) as $file) {
+	$filename= $file->getFilename();
+	if ($filename !="index.php") {
+	  $justname = substr($filename,0,-3);
+		echo "<p><a href='$filename'>$justname</a></p>";	
+	}
+} // END foreach
+
 ?>
 
