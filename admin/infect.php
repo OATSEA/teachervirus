522bca204039d9742c2018a77644c8ced7432d77
harrylongworth <harry@harrylongworth.com>
2015-05-25 22:19:13 +1000

    Update infect.php

--- admin/infect.php
+++ admin/infect.php
@@ -1,7 +1,10 @@
 <?php
 // explain how to be infectious
 
-$myIP = $_SERVER['SERVER_ADDR'];
+// $myIP = $_SERVER['SERVER_ADDR'];
+
+// Alternative from http://stackoverflow.com/questions/3219178/php-how-to-get-local-ip-of-system
+$myIP = getHostByName(getHostName());
 
 echo "<!DOCTYPE html><html><title>Be Infectious</title></head><body>
   <h1>Be Infectious!</h1>
