b5138cb662bb59dc73d7d9b58c5736b084bc8815
harrylongworth <harry@harrylongworth.com>
2015-05-25 21:50:26 +1000

    Create infect.php

---
+++ admin/infect.php
@@ -0,0 +1,13 @@
+<?php
+// explain how to be infectious
+
+$myIP = $_SERVER['SERVER_ADDR'];
+
+echo "<!DOCTYPE html><html><title>Be Infectious</title></head><body>
+  <h1>Be Infectious!</h1>
+  <p>To infect another device provide the owner/administrator with the IP Address:</p>
+  <p><b> $myIP </b></p>
+  <hr>
+  </body></html>
+  ";
+?>
