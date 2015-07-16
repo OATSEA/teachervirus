f5ecba702e294ffcc467de44c5e9bd6b55c0ba88
harrylongworth <harry@harrylongworth.com>
2015-07-17 09:36:06 +1000

    Delete username_password.php

--- admin/username_password.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-        //Encrypted UserName and Password
-        $sUserName = md5("admin");
-        $sPassword = "202cb962ac59075b964b07152d234b70";
-        // Defining UserName and Password to check against credentials
-        define("USER_NAME", $sUserName);
-        define("PASSWORD", $sPassword);
-        ?>
