b72c5e071dc15f4079d6d8ee639f956a921fddd2
Vishal Simon <vishalsimon20121981@gmail.com>
2015-08-25 16:31:51 +0530

    destroy session

--- admin/logout.php
+++ admin/logout.php
@@ -1,5 +1,6 @@
 <?php
     require_once("../data/constants.php");
-    
-    header("Location:".SITE_URL.'/play/');
+    session_start();
+    session_destroy();
+    header("Location:".SITE_URL.'/play');
 ?>
