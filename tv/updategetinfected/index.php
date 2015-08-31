3667097c01cc1636b4b618ff2c8a5c25b1f98c5d
Vishal Simon <vishalsimon20121981@gmail.com>
2015-08-31 09:40:19 +0530

    continue changing with branch

--- tv/updategetinfected/index.php
+++ tv/updategetinfected/index.php
@@ -230,35 +230,6 @@
         
         if($_SESSION['isValidation']['flag'] == 1)
         {
-            $sDocumentRoot = ROOT_DIR;
-            $sSiteUrl = SITE_URL;
-            $sExternalFolder = EXTERNAL_FOLDER;
-            $sExternalPath = EXTERNAL_PATH;
-            $sLanguage = LANGUAGE;
-            $bDebugText = DEBUG_TEXT;
-            $bExternalText = EXTERNAL_TEXT;
-            $sTvBranchName = TV_BRANCH;
-            $bAdminCog = ADMIN_COG;
-            $sGetInfectedBranch = $sBranchName;
-            
-            unlink("$sDocumentRoot/data/constants.php");
-            
-            $sListContent = "<?php
-            define('ROOT_DIR','$sDocumentRoot');
-            define('SITE_URL','$sSiteUrl');
-            define('EXTERNAL_FOLDER','$sExternalFolder');
-            define('EXTERNAL_PATH','$sExternalPath');
-            define('LANGUAGE','$sLanguage');
-            define('DEBUG_TEXT','$bDebugText');
-            define('EXTERNAL_TEXT','$bExternalText');
-            define('TV_BRANCH','$sTvBranchName');
-            define('ADMIN_COG','$bAdminCog');
-            define('GETINFECTED_BRANCH','$sGetInfectedBranch');";
-            
-            $myfile = fopen("$sDocumentRoot/data/constants.php", "w")or die('Cannot open file: constants.php');
-            fwrite($myfile, $sListContent);
-            fclose($myfile);
-            
             function rrmdir($dir)
             {
                if (is_dir($dir)) { 
@@ -914,25 +885,54 @@
                         }
                     }
                 }
-                    $zip->close();
-                    unlink($zipfile);
-                    $installed = 1;
-                    echo "<h2>Update Successfully</h2>";
-                    if (is_dir(ROOT_DIR."/admin")) 
-                    {
-                        echo '<link href="'.SITE_URL.'/css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-                                <div class="color-white">
-                                    <a class="play_img" href="'.$sUrl.'">
-                                        <i class="mainNav fa fa-arrow-circle-left fa-3x"></i>
-                                    </a>
-                                </div><br/><br/>';
-                    }
-                    else
-                    {
-                        echo '<div class="color-white">
-                                    <a href="'.$sUrl.'"" class="go-back">Go Back</a>
-                                </div><br/><br/>';
-                    }
+                $zip->close();
+                unlink($zipfile);
+                $installed = 1;
+                echo "<h2>Update Successfully</h2>";
+                if (is_dir(ROOT_DIR."/admin")) 
+                {
+                    echo '<link href="'.SITE_URL.'/css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+                            <div class="color-white">
+                                <a class="play_img" href="'.$sUrl.'">
+                                    <i class="mainNav fa fa-arrow-circle-left fa-3x"></i>
+                                </a>
+                            </div><br/><br/>';
+                }
+                else
+                {
+                    echo '<div class="color-white">
+                                <a href="'.$sUrl.'"" class="go-back">Go Back</a>
+                            </div><br/><br/>';
+                }
+                
+                $sDocumentRoot = ROOT_DIR;
+                $sSiteUrl = SITE_URL;
+                $sExternalFolder = EXTERNAL_FOLDER;
+                $sExternalPath = EXTERNAL_PATH;
+                $sLanguage = LANGUAGE;
+                $bDebugText = DEBUG_TEXT;
+                $bExternalText = EXTERNAL_TEXT;
+                $sTvBranchName = TV_BRANCH;
+                $bAdminCog = ADMIN_COG;
+                $sGetInfectedBranch = $sBranchName;
+
+                unlink("$sDocumentRoot/data/constants.php");
+
+                $sListContent = "<?php
+                define('ROOT_DIR','$sDocumentRoot');
+                define('SITE_URL','$sSiteUrl');
+                define('EXTERNAL_FOLDER','$sExternalFolder');
+                define('EXTERNAL_PATH','$sExternalPath');
+                define('LANGUAGE','$sLanguage');
+                define('DEBUG_TEXT','$bDebugText');
+                define('EXTERNAL_TEXT','$bExternalText');
+                define('TV_BRANCH','$sTvBranchName');
+                define('ADMIN_COG','$bAdminCog');
+                define('GETINFECTED_BRANCH','$sGetInfectedBranch');";
+
+                $myfile = fopen("$sDocumentRoot/data/constants.php", "w")or die('Cannot open file: constants.php');
+                fwrite($myfile, $sListContent);
+                fclose($myfile);
             } // END Download if zipfile doesn't already exists
         }
     }
