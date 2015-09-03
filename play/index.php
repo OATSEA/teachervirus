ba7dafe54555fd803aa003a169cd4714de01394c
Vishal Simon <vishalsimon20121981@gmail.com>
2015-09-03 10:34:44 +0530

    set urls for payload

--- play/index.php
+++ play/index.php
@@ -26,7 +26,7 @@
             echo '<div class="color-white"><a class="admin_img" href="'.$adminURL.'"><i class="mainNav fa fa-cog fa-3x"></i></a></div><br/><br/>';
         }
         if(glob($payloadsDir, GLOB_ONLYDIR)==FALSE){
-          $path = SITE_URL."/admin/getpayload/";
+          $path = SITE_URL."/admin/getpayload";
            echo '<div><h2> You have Not Installed Any Payloads Please <a href='.$path.'></h2></div><div><font size="6">Click Here</font></div></a>
                  <div><h2> To Install Payloads</h2></div>';
          
