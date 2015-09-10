4bac0b5f2721a464649763a20cbb70b457f5195e
Vishal Simon <vishalsimon20121981@gmail.com>
2015-09-10 18:06:57 +0530

    save infected resource

--- data/bootstrap.php
+++ data/bootstrap.php
@@ -11,7 +11,11 @@
     else
     {
         $sDocumentRoot = getcwd();
+        $sInfectionResource = isset($_SESSION['infection_resource']) ? $_SESSION['infection_resource'] : '';
         $sTvBranchName = isset($_SESSION['teachervirus_branch']) ? $_SESSION['teachervirus_branch'] : 'master';
+        $sDeviceAddress = isset($_SESSION['device_address']) ? $_SESSION['device_address'] : '';
+        $nPort = isset($_SESSION['port_number']) ? $_SESSION['port_number'] : '';
+        
         $bChmod = isset($_SESSION['chmod']) ? $_SESSION['chmod'] : '0';
         $sListContent = "<?php
         define('ROOT_DIR','$sDocumentRoot');
@@ -26,11 +30,13 @@
         define('TV_BRANCH','$sTvBranchName');
         define('GETINFECTED_BRANCH','master');    
         define('ADMIN_COG','1');
-        define('SHOW_TV','0');";
+        define('SHOW_TV','0');
+        define('INFECTED_RESOURCE','$sInfectionResource');
+        define('DEVICE_ADDRESS','$sDeviceAddress');
+        define('PORT_NUMBER','$nPort');";
         
         $myfile = fopen("$sDocumentRoot/data/constants.php", "w")or die('Cannot open file: constants.php');
         fwrite($myfile, $sListContent);
         fclose($myfile);
         require($sDestination);
     }
-?>
