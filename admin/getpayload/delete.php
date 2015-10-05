f846be6c622ade73bff04da2695266beedf3bb31
Vishal Simon <vishalsimon20121981@gmail.com>
2015-10-05 17:55:29 +0530

    remove payload and play icon name

--- admin/getpayload/delete.php
+++ admin/getpayload/delete.php
@@ -1,171 +1,251 @@
-<?php
-    session_start();
+<?php 
+    if(session_status()!=PHP_SESSION_ACTIVE) session_start();
+    error_reporting(E_ALL ^ E_WARNING);
+    require_once("../../data/constants.php");
+    require(ROOT_DIR.'/admin/checkLogin.php');
+
+
+    $sPayloadName = $sPayloadUrl = $sIsAdmin = '';
     function listFolderFiles($dir,$sHiddenName,$sCheckId)
     {
-        $sNewDirPath = $_SERVER['DOCUMENT_ROOT'].'/'.$dir;
+        $sNewDirPath = ROOT_DIR.DIRECTORY_SEPARATOR.$dir;
         $ffs = array();
-        if(is_dir($_SERVER['DOCUMENT_ROOT'].'/'.$dir))
-        $ffs = scandir($_SERVER['DOCUMENT_ROOT'].'/'.$dir);
+        if(is_dir(ROOT_DIR.DIRECTORY_SEPARATOR.$dir))
+        $ffs = scandir(ROOT_DIR.DIRECTORY_SEPARATOR.$dir);
         $nHiddenCnt = 0;
         foreach($ffs as $ff)
         {
             if($dir == "payloads" || $dir == "admin")
             {
-                if(is_dir($_SERVER['DOCUMENT_ROOT'].'/'.$dir.'/'.$ff) && $ff != '.' && $ff != '..')
+                if(is_dir(ROOT_DIR.DIRECTORY_SEPARATOR.$dir.DIRECTORY_SEPARATOR.$ff) && $ff != '.' && $ff != '..')
                 {
-        ?>
-                        <input class="text-left" type='checkbox' id='<?php echo $sCheckId.'_'.$nHiddenCnt ?>' onclick='setHiddenValue("<?php echo $sHiddenName.'_'.$nHiddenCnt ?>","<?php echo $sCheckId.'_'.$nHiddenCnt ?>","<?php echo $sNewDirPath.'/'.$ff ?>");'>
-                        <div class="text-field"><?php echo $ff ?></div>
-                        <input type='hidden' id='<?php echo $sHiddenName.'_'.$nHiddenCnt ?>' name='<?php echo $sHiddenName.'_'.$sHiddenName.'_'.$nHiddenCnt ?>'><br/><br/>
-                    
-        <?php
-                    $nHiddenCnt++;
+                    $extension = pathinfo($ff, PATHINFO_EXTENSION);
+                    if ($extension != 'zip') 
+                    {
+                ?>
+                        <input class="text-left" type='checkbox' id='<?php echo $sCheckId.'_'.$nHiddenCnt ?>' name="delete_payload[]" value="<?php echo $sNewDirPath.DIRECTORY_SEPARATOR.$ff ?>" onClick="getDetails('<?php echo $sCheckId.'_'.$nHiddenCnt ?>')">
+                        <div class="text-field"><?php echo '&nbsp;'.$ff ?></div><br/>
+
+                <?php
+                        $nHiddenCnt++;
+                    }
                 }
             }
-            else
+            
+            if($dir == "content" || $dir == "data")
             {
-                $extension = pathinfo($ff, PATHINFO_EXTENSION);
-                if ($extension == 'zip') {
-        ?>
-                    <input class="text-right" type='checkbox' id='<?php echo $sCheckId.'_'.$nHiddenCnt ?>' onclick='setHiddenValue("<?php echo $sHiddenName.'_'.$nHiddenCnt ?>","<?php echo $sCheckId.'_'.$nHiddenCnt ?>","<?php echo $sNewDirPath.'/'.$ff ?>")'>
-                    <div class="text-field"><?php echo $ff ?></div>    
-                    <input type='hidden' id='<?php echo $sHiddenName.'_'.$nHiddenCnt ?>' name='<?php echo $sHiddenName.'_'.$nHiddenCnt ?>'><br/><br/>
-        <?php
-                        //$_SESSION['nHiddenCnt'] = isset($_SESSION['nHiddenCnt']) ? $_SESSION['nHiddenCnt'] + 1 : $nHiddenCnt + 1;
-                        $nHiddenCnt++;
+                if(is_dir(ROOT_DIR.DIRECTORY_SEPARATOR.$dir.DIRECTORY_SEPARATOR.$ff) && $ff != '.' && $ff != '..')
+                {
+                    if(is_dir(ROOT_DIR.DIRECTORY_SEPARATOR.$dir.DIRECTORY_SEPARATOR.$ff))
+                        $aFolders = scandir(ROOT_DIR.DIRECTORY_SEPARATOR.$dir.DIRECTORY_SEPARATOR.$ff);
+                    
+                    foreach($aFolders as $sFolder)
+                    {
+                        if($sFolder != '.' && $sFolder != '..')
+                        {
+                            $extension = pathinfo($sFolder, PATHINFO_EXTENSION);
+                            if ($extension != 'zip') 
+                            {
+                    ?>
+                            <input class="text-left" type='checkbox' id='<?php echo $sCheckId.'_'.$nHiddenCnt ?>' name="delete_payload[]" value="<?php echo $sNewDirPath.DIRECTORY_SEPARATOR.$ff.DIRECTORY_SEPARATOR.$sFolder ?>" onClick="getDetails('<?php echo $sCheckId.'_'.$nHiddenCnt ?>')">
+                            <div class="text-field"><?php echo '&nbsp;'.$sFolder ?></div><br/>
+
+                    <?php
+                                $nHiddenCnt++;
+                            }
+                        }
+                    }
                 }
             }
         }
     }
-    // RRMDIR: Recursively remove subdirectories function 
-    // SOURCE: taken http://php.net/manual/en/function.rmdir.php 
-    function rrmdir($dir) 
-    {
-        if (is_dir($dir)) { 
-         $objects = scandir($dir); 
-         foreach ($objects as $object) { 
-           if ($object != "." && $object != "..") { 
-             if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object); 
-           } 
-         } 
-         reset($objects);
-         rmdir($dir);
+// RRMDIR: Recursively remove subdirectories function 
+// SOURCE: taken http://php.net/manual/en/function.rmdir.php 
+
+function rrmdir($dir) 
+{
+    if (is_dir($dir)) { 
+     $objects = scandir($dir);
+     foreach ($objects as $object) { 
+       if ($object != "." && $object != "..")
+           { 
+            if (filetype($dir."/".$object) == "dir")
+                rrmdir($dir."/".$object);
+            else unlink($dir."/".$object); 
        } 
-       else
-       {
-           (file_exists($dir)) ? unlink($dir) : '';
-           if (preg_match('/admin/',$dir))
-               rrmdir ($_SERVER['DOCUMENT_ROOT'].'admin');
+     } 
+     reset($objects);
+     rmdir($dir);
+   } 
+   else
+   {
+       (file_exists($dir)) ? unlink($dir) : '';
+       if (preg_match('/admin/',$dir))
+           rrmdir ($_SERVER['ROOT_DIR'].'infect/admin');
+       
+       if (preg_match('/payloads/',$dir))
+           rrmdir ($_SERVER['ROOT_DIR'].'infect/payloads');
+   }
+} // END RRMDIR
 
-           if (preg_match('/payloads/',$dir))
-                   rrmdir ($_SERVER['DOCUMENT_ROOT'].'payloads');
-       }
-    } // END RRMDIR
-
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+    foreach ($_POST['delete_payload'] as $aPost)
+    {
+        (empty($aPost)) ? '' :rrmdir($aPost);
+        (file_exists($aPost.'.zip')) ? unlink($aPost.'.zip') : '';
+    }
+}
     $protocol = isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != "off") ? "https" : "http";
     $protocol .= "://" . $_SERVER['HTTP_HOST'];
-    if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['delete']))
-    {
-        foreach ($_POST as $aPost)
-        {
-            (empty($aPost)) ? '' :rrmdir($aPost);
-        }
-        echo '<h2>Delete Complete!</h2>'
-        . '<div class="admin_img"><a href="'.$protocol.'/admin" class="color-white"><i class="mainNav fa fa-cog fa-3x"></i></a></div>'
-        . '<div class="play_img"><a href="'.$protocol.'/play/" class="color-white"><i class="mainNav fa fa-play-circle-o fa-3x"></i></a></div>';
-     die();
-    }
 ?>
 <html>
     <head>
         <title>Payloads</title>
         <meta charset="utf-8">
-        <link href="../buttons.css" rel="stylesheet">
         <link href="../../css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
         <script src="../../js/jquery.js" type="text/javascript"></script>
         <style>
-                body{
-                    background-color: black;
-                    min-height:800px;
-                    padding: 0px;
-                    margin: 0;
-                    color: #fff;
-                }
-                form{
-                    border: 1px solid #fff;
-                    height: 88%;
-                    margin-left: 25%;
-                    padding: 15px;
-                    width: 50%;
-                }
-                .error-message{
-                    color: red;
+            label {
+            font-weight: normal;
+            line-height: 24px !important;
+            }
+      @media only screen  and (max-width:1366px)
+            {          
+            form{
+                border: 1px solid #fff;
+                float: left;
+                margin: 20px auto;
+                padding: 20px;
+                text-align: left;
+                width: 100%;
+               }
+               select{
+                   width : 100%;
+               }
+            }
+
+            .example-text{
+                width: 100%;
+            }
+            .go-button{
                     float: right;
-                    margin-bottom: 10px;
-                    width: 250px;
-                }
-                .text-field{
-                    padding-right: 10px;
-                    text-align: left;
-                }
-                .text-left{
-                    float: left;
-                }
-                .example-text{
-                    text-align: right;
-                    width: 370px;
-                }
-                .go-button{
-                    float: right;
-                }
-                .go-button > input{
+                    margin-right: 10px;
+                    padding:10px !important;
+            }
+            .go-button > a{
                     color: #000;
-                }
-                .admin_img {
-                    color: #fff;
-                    float: right;
-                    padding-bottom: 10px;
-                    padding-right: 20px;
-                    padding-top: 10px;
-                }
-                .color-white{
-                    color: #fff;
-                    line-height: 15px;
-                }
+                    background: blue !important;
+                    border-radius: 5px;
+                    padding:7px !important;
+                    border: none;
+                    color: white;
+
+            }
+            .go-button > a:hover{
+                color: white;
+                text-decoration: none;
+            }
+            .admin_img {
+                color: #fff;
+                float: right;
+                padding-bottom: 10px;
+                padding-right: 20px;
+                padding-top: 10px;
+            }
+            
+            .payload-details{
+                border-bottom: 1px solid #fff;
+                margin-bottom: 20px;
+                text-align: center;
+                width: 100%;
+            }
+            .mandatory{
+                font-weight: bold;
+                font-size: 18px;
+            }
+            .sources{
+                    margin-left: 13px;
+                    }
+            .text-left{
+                float: left;
+            }
+            .admin-payloads{
+                border-right: 1px solid #fff;
+                float: left;
+                width: 50%;
+            }
+            .general-payloads{
+                border-right: 1px solid #fff;
+                float: right;
+                width: 48%;
+            }
+            .payloads{
+                width: 100%;
+
+            }
+            #loading {
+                font-size: 70px;
+                font-weight: bold;
+                color: #000;
+                width: 100%;
+                height: 100%;
+                top: 0px;
+                left: 0px;
+                position: fixed;
+                display: block;
+                opacity: 0.7;
+                background-color: #fff;
+                z-index: 99;
+                text-align: center;
+            }
+            #loading-image {
+               position: absolute;
+               top: 100px;
+               left: 500px;
+               z-index: 100;
+            }
+            @media only screen and (max-width:1366px)
+            {
                 input[type="text"] {
                     color: #000;
-                    float: left;
-                    width: 30%;
                     display: block;
                     margin-bottom: 10px;
                     background-color: wheat;
-                }
-                .payload-details{
-                    border-bottom: 1px solid #fff;
-                    margin-bottom: 20px;
-                    text-align: center;
+                    width: 24%;
+                }  
+            }
+            @media only screen and (min-width:20px) and (max-width:480px)
+            {
+            input[type="text"] {
+                    color: #000;
+                    display: block;
+                    margin-bottom: 10px;
+                    background-color: wheat;
                     width: 100%;
-                }
-                .mandatory{
-                    font-weight: bold;
-                    font-size: 18px;
-                }
-                .admin-payloads{
-                    border-right: 1px solid #fff;
-                    float: left;
-                    min-height: 500px;
-                    width: 50%;
-                }
-                .general-payloads{
-                    float: right;
-                    min-height: 400px;
-                    width: 48%;
-                }
-                .payloads{
-                    height: 700px;
-                    width: 100%;
-                }
+                    }
+            }
+
+        
+            input[type="radio"] {
+            line-height: normal;
+            margin: 11px 0 0;
+           }
+            
+            .title h2 {
+             margin: auto 0px;
+             text-align: center;
+             border-bottom: 1px solid;
+             margin-bottom: 17px;
+             padding-bottom: 10px;
+            }
+            input[type="checkbox"]{
+                margin: 4px 0px 0px 16px;
+                line-height: normal;
+            }
+           
         </style>
+        
         <script type="text/javascript">
             function setHiddenValue(hiddenFieldId, checkId, dirPath)
             {
@@ -178,41 +258,77 @@
                     $('#'+hiddenFieldId).val('');
                 }
             }
+            
+            function changeValue()
+            {
+                if ($('#ckeck_admin').is(":checked"))
+                {
+                    $('#ckeck_admin').val(1);
+                }
+                else
+                {
+                    $('#ckeck_admin').val(0);
+                }
+            }
+            
+            function deletePayload()
+            {
+                var x = confirm("Are you sure you want to delete payload!!");
+                if (x == true)
+                {
+                    $("#getpayload_form").submit();
+                }
+                return false;
+            }
         </script>
+        <link href="<?php echo SITE_URL; ?>/css/bootstrap.min.css" rel="stylesheet">
+        <link href="<?php echo SITE_URL; ?>/admin/buttons.css" rel="stylesheet">
+        
     </head>
     <body class="main">
         <div class="color-white">
-            <a class="play_img" href="<?php echo $protocol; ?>">
+            <a class="play_img" href="<?php echo SITE_URL; ?>">
                 <i class="mainNav fa fa-arrow-circle-left fa-3x"></i>
             </a>
         </div><br/><br/>
-        <div id="container">
-            <form method="post" name="form1" action="">
+        <div class="container">
+            <form id="getpayload_form" method="post" name="form1" action="" enctype="multipart/form-data" style="width:100%;">
                 <div class="payloads">
                     <div class="payload-details">
                         <h2>Payload Details</h2>
                     </div>
+                    
                     <div class="admin-payloads">
                         <div class="text-field">
                             <h2>Admin Payloads</h2><hr/>
+                            <label class="lable">
                             <?php
-                                listFolderFiles("admin","infect_admin","infect_admin_check");
+                                listFolderFiles("admin","admin","admin_check");
+                                listFolderFiles("infect/admin","infect_admin","infect_admin_check");
+                                
                             ?>
+                            </label>
                         </div>
                     </div>
 
                     <div class="general-payloads">
                         <div class="text-field">
                             <h2>General Payloads</h2><hr/>
+                            <label class="lable">
                             <?php
-                                listFolderFiles("payloads","infect_payloads","infect_payloads_check");
+                                listFolderFiles("payloads","payloads","payloads_check");
+                                listFolderFiles("content","content","content_check");
+                                listFolderFiles("data","data","data_check");
+                                listFolderFiles("infect/payloads","infect_payloads","infect_payloads_check");
                             ?>
+                            </label>
                         </div>
                     </div>
                     <div class="go-button">
-                        <input type="submit" name="delete" id="delete" value="Delete">
+                        <a href="javascript:void(0);" onclick="deletePayload()">Delete</a>
                     </div>
                 </div>
+                <br/>
             </form>
         </div>
     </body>
--- play/index.php
+++ play/index.php
@@ -72,10 +72,11 @@
             else
             {
                 // Icon provided so use the default
+                
                 echo '<div class="full-width"><div class="half-width"<a href="'.$payloadsURL.'/'.$dir.'"><img class="mybutton" alt="'.$dir.'" src="default.svg" />';
-                $aPayloadLabel = explode('-',$dir);
+                $aPayloadLabel = explode('.',$dir);
                 echo '<label class="payload-label">';
-                echo (PAYLOAD_LABEL == 1) ? substr($aPayloadLabel[2],0,13) : '';
+                echo (PAYLOAD_LABEL == 1) ? substr($aPayloadLabel[0],0,13) : '';
                 echo '</label></a>';
                 echo '</div></div>';
             }
