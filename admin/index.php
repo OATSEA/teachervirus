19c9852a8234d2b6ca61634a877e887b5a84d86e
Vishal Simon <vishalsimon20121981@gmail.com>
2015-12-14 14:18:12 +0530

    update admin and play index

--- admin/index.php
+++ admin/index.php
@@ -2,19 +2,16 @@
 <html lang="en-US">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1;url=tv/admin/">
-<?php   if (is_dir(getcwd()."/admin"))   
-        { 
-            header('Location: /tv/admin');
-        }
-        
+        <?php   
+
+            $url=getcwd();
+            $constant = explode('/admin',$url);
+            header('location:/tv/admin');
         ?> 
-            
-  
-        <title>Page Redirection</title>
+    <title>Page Redirection</title>
     </head>
     <body>
         <!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
-        If you are not redirected automatically <a href='tv/admin/'>click here</a>
+       
     </body>
 </html>
--- play/index.php
+++ play/index.php
@@ -2,19 +2,15 @@
 <html lang="en-US">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1;url=tv/play/">
-<?php   if (is_dir(getcwd()."/play"))   
-        { 
-            header('Location: /tv/play/');
-        }
-        
+        <?php   
+
+            $url=getcwd();
+            $constant = explode('/play',$url);
+            header('location:/tv/play');
         ?> 
-            
   
         <title>Page Redirection</title>
     </head>
     <body>
-        <!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
-        If you are not redirected automatically <a href='tv/play/'>click here</a>
     </body>
 </html>
